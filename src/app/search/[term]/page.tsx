13021602c187fbc4071178f37b4fa3724b3c6824
Alican Şan <alicansan2606@gmail.com>
2024-07-01 00:11:09 +0300

    asd

--- src/app/search/[term]/page.tsx
+++ src/app/search/[term]/page.tsx
@@ -1,62 +1,62 @@
-// import { notFound } from 'next/navigation'
-// import { useEffect, useState } from 'react'
+import { notFound } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
-// type Props = {
-//   params: {
-//     term: string
-//   }
-// }
+type Props = {
+  params: {
+    term: string
+  }
+}
 
-// function Search({ params: { term } }: Props) {
-//   if (!term) notFound()
+function Search({ params: { term } }: Props) {
+  if (!term) notFound()
 
-//   const [results, setResults] = useState<any[]>(
-//     []
-//   ) //hodls the search results
+  const [results, setResults] = useState<any[]>(
+    []
+  ) //hodls the search results
 
-//   const [isLoading, setIsLoading] =
-//     useState<boolean>(true) //holds the loading state
+  const [isLoading, setIsLoading] =
+    useState<boolean>(true) //holds the loading state
 
-//   const [error, setError] = useState<
-//     string | null
-//   >(null) //holds the error state
+  const [error, setError] = useState<
+    string | null
+  >(null) //holds the error state
 
-//   const termToUse = decodeURI(term) //decoding the seeach term
+  const termToUse = decodeURI(term) //decoding the seeach term
 
-//   useEffect(() => {
-//     const fetchSearchResults = async () => {
-//       try {
-//         setIsLoading(true)
+  // useEffect(() => {
+  //   const fetchSearchResults = async () => {
+  //     try {
+  //       setIsLoading(true)
 
-//         const response = await fetch(
-//           `https://api.themoviedb.org/3/search/multi?query=${termToUse}&include_adult=false&language=en-US&page=1`,
-//           {
-//             method: 'GET',
-//             headers: {
-//               accept: 'application/json',
-//               Authorization:
-//                 'Bearer 0c7f3bc0a6eed5d16f7e2de13906753a',
-//             },
-//           }
-//         )
+  //       const response = await fetch(
+  //         `https://api.themoviedb.org/3/search/multi?query=${termToUse}&include_adult=false&language=en-US&page=1`,
+  //         {
+  //           method: 'GET',
+  //           headers: {
+  //             accept: 'application/json',
+  //             Authorization:
+  //               'Bearer 0c7f3bc0a6eed5d16f7e2de13906753a',
+  //           },
+  //         }
+  //       )
 
-//         const data = await response.json()
+  //       const data = await response.json()
 
-//         setResults(data.results)
-//       } catch (err) {
-//         setError(
-//           'Arama sonuçları yüklenirken bir hata oluştu.'
-//         )
-//       } finally {
-//         setIsLoading(false)
-//       }
-//     }
+  //       setResults(data.results)
+  //     } catch (err) {
+  //       setError(
+  //         'Arama sonuçları yüklenirken bir hata oluştu.'
+  //       )
+  //     } finally {
+  //       setIsLoading(false)
+  //     }
+  //   }
 
-//     // Arama isteğini yapma
-//     fetchSearchResults()
-//   }, [termToUse])
+  //   // Arama isteğini yapma
+  //   fetchSearchResults()
+  // }, [termToUse])
 
-//   return <div>{termToUse}</div>
-// }
+  return <div>{termToUse}</div>
+}
 
-// export default Search
+export default Search
